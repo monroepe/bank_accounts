e8db82e344e1b0daba2b660cf5d422f05e4ca991
Peter <monroepe@gmail.com>
2014-09-04 11:38:11 -0400

    Transaction class created

--- transaction.rb
+++ transaction.rb
@@ -1,17 +1,23 @@
+require 'pry'
+
 class Transaction
 
-  def initialize
+  attr_reader :type, :date, :amount, :source
 
+  def initialize(amount, date, type, source)
+    @amount = amount.to_f
+    @date = date
+    @type = type
+    @source = source
   end
 
   def deposit?
-
+    type.downcase == 'deposit'
   end
 
   def summary
-
+    "$#{amount}  #{type.upcase}  #{date} - #{source}"
   end
 
 end
-
 
