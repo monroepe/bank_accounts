c1dbdabbab4e7252de5b6e8e7d0bf7fe8f880de3
Peter <monroepe@gmail.com>
2014-09-04 11:57:10 -0400

    reads in and returns starting balance

--- account.rb
+++ account.rb
@@ -1,11 +1,24 @@
+require 'pry'
+require 'csv'
+
 class Account
 
-  def initialize
+  attr_reader :name, :balance_data, :transactions_data
 
+  def initialize(name, balance_data, transactions_data)
+    @name = name
+    @balance_data = balance_data
+    @transactions_data = transactions_data
   end
 
   def starting_balance
-
+    balance = 0
+    CSV.foreach(balance_data, headers: true, header_converters: :symbol) do |row|
+      if row[:account] == name
+        balance = row[:balance]
+      end
+    end
+    balance
   end
 
   def current_balance
@@ -17,3 +30,6 @@ class Account
   end
 
 end
+
+acct = Account.new("Business Checking", 'balances.csv', 'bank_data.csv')
+binding.pry
