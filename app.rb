50d2800ac9dd64d911e0dd0de33455bd3e8b53d6
Peter <monroepe@gmail.com>
2014-09-04 17:25:39 -0400

    Links to individual accounts

--- app.rb
+++ app.rb
@@ -2,21 +2,44 @@ require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'account'
 require_relative 'transaction'
-
+require 'csv'
 require 'pry'
 
 
+def account_names
+  names = []
+  CSV.foreach('balances.csv', headers: true, header_converters: :symbol) do |row|
+    names << row[:account]
+  end
+  names
+end
 
+def accounts
+  accounts = []
+  account_names.each do |name|
+    accounts << Account.new(name, 'balances.csv', 'bank_data.csv')
+  end
+  accounts
+end
+
+def get_account(name)
+  accounts.each do |account|
+    if account.name == name
+      return account
+    end
+  end
+end
 
 
 get '/' do
   erb :index
 end
 
-get '/account' do
-  erb :'account/index'
+get '/accounts' do
+  erb :'accounts/index', locals: {accounts: accounts}
 end
 
-get '/account/:name' do
-  erb :'account/show'
+get '/accounts/:account_name' do
+  account = get_account(params[:account_name])
+  erb :'accounts/show', locals: {account: account}
 end
